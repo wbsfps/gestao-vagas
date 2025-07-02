8a0c4190b14d56596b9f4bb06229a5da4acb61dd
William Santana <williamandrade1058@gmail.com>
2025-07-02 11:16:19 -0300

    start project

---
+++ src/test/java/br/com/wbs/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.wbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
