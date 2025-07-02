2e0d056a89466530a2ca32ab77020b60ed0e918c
William Santana <williamandrade1058@gmail.com>
2025-07-02 12:07:16 -0300

    entidade company

--- src/main/java/br/com/wbs/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/wbs/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -1,9 +1,6 @@
-package br.com.wbs.gestao_vagas.modules.candidate;
+package br.com.wbs.gestao_vagas.modules.company.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -14,14 +11,12 @@ import org.hibernate.validator.constraints.Length;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity(name = "candidate")
+@Entity(name = "company")
 @Data
-public class CandidateEntity {
-
+public class CompanyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private String name;
 
     @NotBlank
     @Pattern(regexp = "\\S+", message = "O [username] não deve conter espaço")
@@ -32,8 +27,8 @@ public class CandidateEntity {
 
     @Length(min = 10, max = 100, message = "A senha deve ter entre (10) e (100) caracteres")
     private String password;
+    private String website;
     private String description;
-    private String curriculum;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
