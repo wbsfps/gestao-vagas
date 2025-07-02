7cb260bf6c391c3c58904f0fae006cb3511e8364
William Santana <williamandrade1058@gmail.com>
2025-07-02 12:30:15 -0300

    Entidade Job

--- src/main/java/br/com/wbs/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/wbs/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,4 +1,27 @@
 package br.com.wbs.gestao_vagas.modules.company.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
 public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
