956565ed657fa344112a7af8404102657b4a81f8
William Santana <williamandrade1058@gmail.com>
2025-07-03 00:00:13 -0300

    Job Controller

--- src/main/java/br/com/wbs/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/wbs/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,6 +1,7 @@
 package br.com.wbs.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -16,13 +17,15 @@ public class JobEntity {
     private UUID id;
     private String description;
     private String benefits;
+
+    @NotBlank(message = "Esse campo é obrigatório")
     private String level;
 
     @ManyToOne
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
-    @Column(name = "company_id")
+    @Column(name = "company_id", nullable = false)
     private UUID companyId;
 
     @CreationTimestamp
