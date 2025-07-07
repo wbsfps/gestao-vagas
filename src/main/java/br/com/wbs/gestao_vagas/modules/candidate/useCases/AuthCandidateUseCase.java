efdb9a8e4d7929610bfd46fabeb440b17a368273
William Santana <williamandrade1058@gmail.com>
2025-07-07 12:52:31 -0300

    Controller: AuthCandidate

--- src/main/java/br/com/wbs/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/br/com/wbs/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -37,15 +37,18 @@ public class AuthCandidateUseCase {
        }
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
-
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
        var token = JWT.create()
                .withIssuer("javagas")
                .withSubject(candidate.getId().toString())
                .withClaim("roles", Arrays.asList("candidate"))
-               .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+               .withExpiresAt(expiresIn)
                .sign(algorithm);
 
-        var authCandidateResponse = AuthCandidateResponseDTO.builder().access_token(token).build();
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
         return authCandidateResponse;
     }
 }
