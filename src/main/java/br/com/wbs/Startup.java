c195792cae297eba9f045a201f5958972161ee06
William Santana <williamandrade1058@gmail.com>
2025-07-13 11:26:02 -0300

    Configurando swagger

--- src/main/java/br/com/wbs/Startup.java
+++ src/main/java/br/com/wbs/Startup.java
@@ -1,9 +1,15 @@
 package br.com.wbs;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Gestão de vagas", description = "API responsável pela gestão de vagas",
+		version = "1")
+)
 public class Startup {
 
 	public static void main(String[] args) {
