a1c824e5426300864820dbd54f7005d72f0c0c5a
William Santana <williamandrade1058@gmail.com>
2025-07-17 16:26:40 -0300

    Testando exceções

---
+++ src/test/java/br/com/wbs/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,36 @@
+package br.com.wbs.modules.candidate.useCases;
+
+import br.com.wbs.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.wbs.gestao_vagas.modules.candidate.repository.CandidateRepository;
+import br.com.wbs.gestao_vagas.modules.candidate.useCases.ApplyJobCandidateUseCase;
+import br.com.wbs.gestao_vagas.modules.company.repositories.JobRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+    @Mock
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("Should not be able to apply job with candidate not found")
+    public void should_not_be_able_to_apply_job_with_candidate_not_found() {
+        try {
+            applyJobCandidateUseCase.execute(null, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+}
